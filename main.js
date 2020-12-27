07ecb52f8b1f8d6603987ac58f07e21a2d124e27
C-ArenA <carlostata.ca@gmail.com>
2020-12-26 22:06:30 -0400

    Now I've added range inputs to interact with padding and margin

--- main.js
+++ main.js
@@ -5,20 +5,16 @@ paddingText.innerHTML = "Padding ↕ " + style.padding;
 marginText.innerHTML = "Margin ↕ " + style.margin;
 
 function changePadding() {
-    padding += 1;
+    padding = paddingChanger.value;
     inblock.style.padding = padding.toString() + "px";
     paddingText.innerHTML = "Padding ↕ " + inblock.style.padding;
-    if (padding == 50) {
-        padding = 20;
-    }
 }
 function changeMargin() {
-    margin += 1;
+    margin = marginChanger.value;
     inblock.style.margin = margin.toString() + "px";
     marginText.innerHTML = "Margin ↕ " + inblock.style.margin;
-    if (margin == 50) {
-        margin = 20;
-    }
 }
 paddingText.addEventListener("click", changePadding);
 marginText.addEventListener("click", changeMargin);
+paddingChanger.addEventListener("input", changePadding);
+marginChanger.addEventListener("input", changeMargin);
